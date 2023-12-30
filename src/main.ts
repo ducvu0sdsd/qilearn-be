7dbd89ddab13486a9158d97b7353d2e9d4a0ed0a
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-30 16:00:13 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { NestExpressApplication } from '@nestjs/platform-express';
+// import { ValidationPipe } from '@nestjs/common';
+// import * as cookieParser from 'cookie-parser';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(new ValidationPipe())
+//   app.use(cookieParser());
+//   app.enableCors({
+//     origin: 'https://qilearn-nu.vercel.app',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     credentials: true,
+//     allowedHeaders: 'Content-Type, Authorization',
+//   });
+//   await app.listen(8080);
+// }
+bootstrap();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  app.use(cookieParser());
-  app.enableCors({
-    origin: 'https://qilearn-nu.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Authorization',
-  });
-  await app.listen(8080);
+  await app.listen(3000);
 }
 bootstrap();
+
