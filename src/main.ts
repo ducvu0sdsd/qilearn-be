72a2ab21afe1285c178ac97495b9afe3713378b8
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-30 13:15:35 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
-import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
-  app.use(cookieParser());
-  app.enableCors({
-    origin: 'https://qilearn-nu.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    allowedHeaders: 'Content-Type, Authorization',
-  });
-  await app.listen(8080);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
