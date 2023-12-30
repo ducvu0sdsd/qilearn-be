08c424a4c76ab9bd4831aa9b1d64c321dda7acd8
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-31 00:11:18 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
-import * as cookieParser from 'cookie-parser';
+// import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 export async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
