3cccf97b30264ba7735253c3f4b4740e7bbc7bd0
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2023-12-31 00:15:42 +0700

    upload

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ export async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser());
   app.enableCors({
-    origin: 'https://qilearn-nu.vercel.app',
+    origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization',
