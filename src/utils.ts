81eed2f81a3a12fdf47dd04e170bfa56ddcb0e3c
Vũ Tiến Đức <100019107+ducvu0sdsd@users.noreply.github.com>
2024-06-13 22:54:23 +0700

    upload

--- src/utils.ts
+++ src/utils.ts
@@ -3,11 +3,12 @@ import { SubtitleInterface } from "./broadcast/schema/broadcast.schema"
 
 export const handleSRTFile: (str: string) => SubtitleInterface[] = (str) => {
     const arr: string[] = str.split('\n')
+    // console.log(arr)
     const subtitles: SubtitleInterface[] = []
     let subtitle: SubtitleInterface = { id: 0, firstTime: 0, lastTime: 0, content: '' }
     arr.forEach((item1, index) => {
         const item = item1
-        if (/[0-9]{1,6}\r/.test(item) && !item.includes(' --> ')) { // id
+        if (/[0-9]{1,6}/.test(item) && !item.includes(' --> ')) { // id
             const i = parseInt(item.replace('\r', ''))
             subtitle.id = i
         } else if (item.includes(' --> ')) { // time
@@ -23,19 +24,18 @@ export const handleSRTFile: (str: string) => SubtitleInterface[] = (str) => {
             })
         } else if (/[A-ZÀ-Ỹa-zà-ỹ0-9]+/.test(item.replace('- ', '')) && !item.includes(' --> ')) {
             subtitle.content += (' ' + item.replace('\r', '').replace('<i>', '').replace('</i>', ''))
-        } else if (item === '\r') {
+        } else if (item === '') {
             if (subtitle.id !== 0) {
                 subtitles.push(subtitle)
                 subtitle = { id: 0, firstTime: 0, lastTime: 0, content: '' }
             }
         }
-    })
 
+    })
     return subtitles;
 }
 
 const timeStringToSeconds = (timeString: string) => {
-    console.log('0,' + timeString.split(',')[1])
     const milisecond: number = parseFloat('0.' + timeString.split(',')[1])
     const [hours, minutes, seconds] = timeString.split(':').map(parseFloat);
     return hours * 3600 + minutes * 60 + seconds + milisecond;
